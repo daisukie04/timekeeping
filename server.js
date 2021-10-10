1d7b30c165fe4f8acb59b76e264a7a84ee3836b5
Mariel Cruz <alfredobulantecruz@gmail.com>
2021-10-11 00:46:22 +0800

    added env file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use("/login", login)
 
 const port = 3000
-app.listen(port,()=>{
+app.listen(process.env.PORT || port,()=>{
 
     console.log(`app is listening in port ${port}`)
 
